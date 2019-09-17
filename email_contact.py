8847553c62c711a2b087229c43ae839b0a4f879e
unknown <mohammadali1ali@gmail.com>
2019-09-17 18:45:36 +0530

    updated phone number and email extraction

---
+++ email_contact.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 17:11:01 2019
+
+@author: Ali
+"""
+import re
+
+
+
+file = open('resume.txt', encoding="utf8")
+my_corpus = file.read()
+
+
+email_pattern = re.compile(r'[a-zA-Z0-9_\.]+@[a-zA-Z-\.]*\.(com|edu|net|us|org|eu|co.in|in)')
+phone_pattern = re.compile(r'[(][\d]{3}[)] -[]?[\d]{3}-[\d]{4}')
+email_matches = email_pattern.finditer(my_corpus)
+number_matches = phone_pattern.finditer(my_corpus)
+
+for emails in email_matches:
+    print(emails)
+    
+for number in number_matches:
+    print(number)
+    
+    
+
+
+    
+    
+
+
--- parsing.py
+++ parsing.py
@@ -6,3 +6,37 @@ Created on Mon Sep 16 21:41:18 2019
 """
 
 import re
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
+from nltk.corpus import wordnet
+
+
+
+file = open('resume.txt', encoding="utf8")
+my_corpus = file.read()
+
+
+Sentences = nltk.sent_tokenize(my_corpus)
+Tokens = []
+for Sent in Sentences:
+    Tokens.append(nltk.word_tokenize(Sent)) 
+Words_List = [nltk.pos_tag(Token) for Token in Tokens]
+
+Nouns_List = []
+
+for List in Words_List:
+    for Word in List:
+        if re.match('[NN.*]', Word[1]):
+             Nouns_List.append(Word[0])
+
+Names = []
+for Nouns in Nouns_List:
+    if not wordnet.synsets(Nouns):
+        Names.append(Nouns)
+
+print (Names)
